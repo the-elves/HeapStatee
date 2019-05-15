475c98164e5150eb125ae26dd101bc2392e38606
aj <the-elves@bitbucket.org>
2019-05-15 21:32:07 +0530

    free started

--- mstate.py
+++ mstate.py
@@ -12,16 +12,20 @@ STARTING_SIZE = 4096
 MALLOC_ALIGN_MASK = MALLOC_ALLIGNMENT - 1
 MIN_SIZE = (32 + MALLOC_ALIGN_MASK) & ~ MALLOC_ALIGN_MASK
 PREV_INUSE = 1
+MAX_ITERATIONS = 10000
 
 class Chunk:
     size = None
     prev_size = None
     address = None
     fd = None
+    bin = None
     bk = None
     free = True
+    is_mmapped = False
     user_address = None
     is_top = False
+    bin = None
 
 class HeapState:
     allocated_chunks = []
@@ -176,17 +180,20 @@ class HeapState:
         if (nb <= MAX_FASTBIN_SIZE):
             victim = self.allocate_from_fastbin()
             if (victim != None):
+                self.allocated_chunks.append(victim)
                 return victim
         elif (nb <= MIN_LARGE_SIZE):
             victim = self.allocate_from_smallbin()
             if (victim != None):
+                self.allocated_chunks.append(victim)
                 return victim
-        size = victim.size
         self.consolidate()
         while(True):
             while(len(self.unsortedbin) !=0):
                 victim = self.unsortedbin[-1]
-                bck = victim.bck
+                size = victim.size
+                #todo add checks : malloc(): memory corruptio
+                bck = victim.bk
                 if (nb <= MAX_SMALLBIN_SIZE and\
                         self.unsortedbin[0] == bck and\
                         victim == self.lastremainder and\
@@ -194,7 +201,7 @@ class HeapState:
                     remainder_size = size-nb
                     remainder = Chunk
                     remainder.size  = remainder_size
-                    remainder.address = victim.address + size
+                    remainder.address = victim.address + nb
                     del self.unsortedbin[0]
                     self.unsortedbin[0] = remainder
                     self.lastremainder = remainder
@@ -202,8 +209,46 @@ class HeapState:
                     #TODO handle main arena
                     victim.size = size | PREV_INUSE
                     #TODO handle bk and fd
+                    self.allocated_chunks.append(victim)
                     return victim
-                
+                del(self.unsortedbin[-1])
+                if size == nb:
+                    # XXX this chunks inuse set instead of next chunks prev_inuse
+                    victim.free=False
+                    #TODO add checks : check_malloced_chunk(av, victim, nb);
+                    self.allocated_chunks.append(victim)
+                    return victim
+
+                #place chunk in bins
+                if size <= MAX_SMALLBIN_SIZE:
+                    victim_idx = self.smallbin_index(size)
+                    bin = self.smallbin[victim_idx]
+                    bin.append(victim)
+                    victim.bin = bin
+                iteration = iteration + 1
+                if(iteration > MAX_ITERATIONS):
+                    break;
+
+                #TODO handle largebin
+
+                #use top
+                victim = self.top
+                size = victim.size
+                if(size>nb+MIN_SIZE):
+                    remainder_size = size - nb
+                    remainder = Chunk
+                    remainder.size = remainder
+                    remainder.address = victim.address+nb
+                    remainder.free = True
+                    self.top = remainder
+                    victim.size = nb
+                    victim.free = False
+                    self.allocated_chunks.append(victim)
+                    return victim
+
+                #sysmalloc
+                self.top.size = self.top.size + 0x21000
+
 
 
 
@@ -212,10 +257,49 @@ class HeapState:
         self.rebin_unsorted_chunks()
         victim = self.allocate_from_unsorted()
         if(victim != None):
+
             return victim
 
 
 
 
-    def free(self, y):
-        pass
+    def free(self, p):
+        #TODO add security checks
+
+        for c in self.allocated_chunks:
+            if p == c.address:
+                p_chunk = c
+            size = p_chunk.size
+        if p_chunk.size < MAX_FASTBIN_SIZE:
+            #TODO add checks: "free(): invalid next size (fast)"
+            idx = self.get_fast_bin_index(p_chunk.size)
+            fb = self.fastbin[idx]
+            p_chunk.free = True
+            fb = [p_chunk]+fb
+            p_chunk.bin = fb
+            #XXX POSSIBLE ERROR: NEXT AND PREVIOUS NOT UPDATED, INHERENTLY MAINTAINED BY THE LIST
+            #TODO add checks : double free or corruption (fasttop)
+            #TODO MULTIPLE THREADS
+            #TODO add checks : invalid fastbin entry (free)
+        if not p_chunk.is_mmapped:
+            #TODO: multiple thereads
+            next_chunk = self.get_free_chunk_at_offset(p_chunk.address, p_chunk.size)
+            #TODO add checks : double free or corruption (top), (out), (!prev)
+            nextsize = next_chunk.size
+            #TODO add checks : free(): invalid next size (normal)
+            prev_chunk = self.get_free_chunk_at_offset(p_chunk.address, p_chunk.address - p_chunk.prev_size )
+            #XXX probable error : previous inuse checked hackily
+            # instead of checking current chunks prev_inuse and we are checking if the chunk is present in free lists
+            if prev_chunk != None:
+                prev_chunk_bin = prev_chunk.bin
+                prev_idx = prev_chunk_bin.index(prev_chunk)
+                prev_size = prev_chunk.size
+                size = size + prev_size
+                address = prev_chunk.address
+                del(prev_chunk_bin[prev_idx])
+                
+
+
+
+
+
