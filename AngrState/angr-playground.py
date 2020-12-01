32874d833a60919ba47876c7d27932e971af2ae6
ajinkya <ajinkya@abc.in>
2020-12-01 15:37:19 +0530

    filesystem bugs

--- AngrState/angr-playground.py
+++ AngrState/angr-playground.py
@@ -12,6 +12,7 @@ from datetime import datetime
 import signal
 import pdb
 from angr.exploration_techniques import DFS
+import os
 
 HOUR = 60*60
 time_limit = HOUR/4
@@ -45,6 +46,7 @@ def initialize_project(b, ss):
     b.hook_symbol('perror', Perror())
     ss.inspect.b('mem_write', when=angr.BP_BEFORE, action=bp_action_write)
     initialize_logger(b.filename)
+    setup_filesystem(ss)
     
     
 def handle_heap_write(state):
@@ -87,6 +89,18 @@ def bp_action_write(state):
     if addr_in_heap(write_address, state.my_heap.heap_state):
         handle_heap_write(state)
 
+
+def setup_filesystem(estate):
+    host_file_system = angr.SimHostFilesystem('/home/ajinkya/Guided_HLM/guest_chroot/')
+    host_file_system.set_state(estate)
+    estate.fs.mount('/exploits/',host_file_system)
+    
+    simfilename = 'mysimfile'
+    simfile = angr.SimFile(simfilename, size=5*1024*1024)
+    simfile.set_state(estate)
+    estate.fs.insert('/sym-files/', simfile)
+
+    
 def alarm_handler(signum, frame):
     exit()
         
@@ -97,6 +111,10 @@ def stopping_condition():
 
     return False
 
+def print_libs(p):
+    for k,v in p.loader.shared_objects.items():
+        print(f'{k} : {v} : {v.binary}')
+        
 
 next_stopping_addr = -1
 def pdb_stopping_condition():    
@@ -107,29 +125,25 @@ def pdb_stopping_condition():
     
 
 binary_name = sys.argv[1]
+loader_libraries = ['../../tools/glibc-dir/install/lib64/libc-2.27.so',
+                                  '/home/ajinkya/Guided_HLM/tools/glibc-dir/install/lib64/ld-2.27.so']
+if 'HEAPSTATE_LIBS' in os.environ.keys():
+    loader_libraries.append(os.environ['HEAPSTATE_LIBS'])
+
 b = angr.Project(binary_name, auto_load_libs=True,
                  use_sim_procedures=False,
-                 force_load_libs=['../../tools/glibc-dir/install/lib64/libc-2.27.so',
-                                  '/home/ajinkya/Guided_HLM/tools/glibc-dir/install/lib64/ld-2.27.so',
-                                  '/home/ajinkya/Guided_HLM/tools/openjpeg/build/bin/libopenjp2.so.2.3.1']
+                 force_load_libs=loader_libraries
                  )
-print(b.loader.shared_objects)
 
-num_sym_bytes = 20
-input_chars = [claripy.BVS(f'flag{i}',8) for i in range(num_sym_bytes)] + [claripy.BVV('\n', 8)]
-argvinp = claripy.Concat(*input_chars)
-
-# estate = b.factory.entry_state(args = [binary_name, '/f'])
-# simfilename = 'mysimfile'
-# simfile = angr.SimFile(simfilename, size=5*1024*1024)
-# simfile.set_state(estate)
-# estate.fs.insert('/f', simfile)
+print_libs(b)
 
 
-estate = b.factory.entry_state(args = sys.argv[1:])
-host_file_system = angr.SimHostFilesystem('/home/ajinkya/Guided_HLM/guest_chroot/')
-host_file_system.set_state(estate)
-estate.fs.mount('/home/user/exploits/',host_file_system)
+# num_sym_bytes = 20
+# input_chars = [claripy.BVS(f'flag{i}',8) for i in range(num_sym_bytes)] + [claripy.BVV('\n', 8)]
+# argvinp = claripy.Concat(*input_chars)
+
+
+estate = b.factory.entry_state(args = sys.argv[1:], model='tracing')
 
 # estate = b.factory.entry_state(argc = 2, argv = [binary_name, input_chars])
 # for i in range(num_sym_bytes-1):
@@ -140,34 +154,50 @@ initialize_project(b, estate)
 m = b.factory.simulation_manager(estate)
 m.use_technique(DFS())
 # pdb.set_trace()
+progress=0
+
+m.run()
+for s in m.deadended:
+    print('posix out', str(s.posix.dumps(1)))
+    print('posix err', str(s.posix.dumps(2)))
+exit()
+
 while len(m.active) > 0:
     now = datetime.now()
     timestr = now.strftime("%H:%M:%S")
-    print(timestr, 'active states = ',len(m.active), end='         ')
+    addr = m.active[0].solver.eval(m.active[0].regs.ip)
+    print(timestr, 'active states = ', len(m.active), 'rip = ', hex(addr))
     if True:
         try:
             for s in m.active:
                 print("=============>", m.active)
                 s.my_heap.heap_state.dump()
+                print()
                 s.block().pp()
                 print(s.callstack)
-                if pdb_stopping_condition():
-                    pdb.set_trace()     
+            if pdb_stopping_condition():
+                pdb.set_trace()     
         except:
             print("Disassembly not available")
     if(not stopping_condition()):
         m.step()
         try:
-            print(', posix out', str(m.active[0].posix.dumps(1)), '\r', end='')
+            
+            print('posix out', str(m.active[0].posix.dumps(1)))
+            print('posix err', str(m.active[0].posix.dumps(2)))
             # for es in m.errored:
             #     print('Errored: ', es.error)
             #     vl.warning('Errored: ' + str(es.error))
-                    
+            pass        
         except:
             print('\nno output error')
     else:
         print('System memory too low, exiting')
         break
+
+for s in m.deadended:
+    print(f'Out: {s.posix.dumps(1)}')
+
 if len(m.errored) > 0:
     print("============== Errored States ======================")
     for es in m.errored:
