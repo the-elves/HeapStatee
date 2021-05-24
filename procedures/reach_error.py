8941f4bbc484835c1a580620e9ecc0b984f85ce4
aj <the-elves@bitbucket.org>
2021-05-24 19:03:14 +0530

    added reach error simproc

--- procedures/reach_error.py
+++ procedures/reach_error.py
@@ -4,6 +4,6 @@ from utils.utils import radar_breakpoint
 vl = logging.getLogger('vuln_logger')
 class reach_error(angr.SimProcedure):
     def run(self):
-        vl.log("reach error reached")
+        vl.warning("reach error reached")
         print("reach error reached")
         radar_breakpoint()
